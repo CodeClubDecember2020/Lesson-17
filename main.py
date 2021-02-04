47739ce83078c6fa51ab1d7aefc4a713188bf9dc
Samu B <samubalogh@gmail.com>
2021-02-04 12:15:30 +0000

    make them fight!

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ print('crocodile')
 # what is the problem here?
 result = 2 + 2
 
+
+# Exceptions can happen at any place in our code, not just where we expect them.
+# Sometimes, they are expected, sometimes, they 
+
 # iterate over the list and print the half of every item
 # if the item cannot be divided (due to TypeError), print "Not a number"
 list_of_things = [2, 2, 'banana', 'superman', 30.0, 416]
@@ -76,18 +80,72 @@ superheroes = {
 # this is a class
 class SuperHero:
     # this is the "constructor" method - the __init__ method
-    def __init__(self, name, powers, legal_name=None):
+    def __init__(self, name, powers, strength, legal_name=None):
         self.name = name
         self.powers = powers
         self.legal_name = legal_name
+        self.strength = strength
+        self.life = 20
+        self.max_life = 20
 
         self.description = self.name + ' has the following powers: ' + ', '.join(self.powers)
         if self.legal_name is not None:
-            description += ' and their legal name is ' + self.legal_name
+            self.description += ' and their legal name is ' + self.legal_name
 
     # this is an "instance method"
     def get_description(self):
         return self.description
+
+    def speak(self):
+        pass
+
+    def fight(self, other):
+
+        def check_if_sick(self, other):
+            result = False
+            if self.life <= self.max_life//5:
+                print(f'They will not fight - {self.name} is sick')
+                result = True
+            elif other.life <= other.max_life//5:
+                print(f'They will not fight - {other.name} is sick')
+                result = True
+            if result is True:
+                print('They will have to go to the hospital 🏥 🚑 👩‍⚕️')
+
+            return result
+            
+        is_anyone_sick = check_if_sick(self, other)
+
+        if is_anyone_sick:
+            return
+
+        print(f'Friendly fight: {self.name} VS {other.name}')
+        print('-------------')
+
+        if other.strength > self.strength:
+            self.life //= 2
+            print(f'Result: {self.name} was beaten by {other.name} 💥')
+
+        elif other.strength < self.strength:
+            print(f'{self.name} has beaten {other.name} 💥')
+            other.life //= 2
+
+        print(f'{self.name} has {self.life} health points left')
+        print(f"{self.life * '💚'}")
+        print()
+        print(f'{other.name} has {other.life} health points left')
+        print(f"{other.life * '💚'}")
+
+
+# another approach is to create a Fight class:
+class Fight:
+    def __init__(self, player_one, player_two):
+        self.p1 = player_one
+        self.p2 = player_two
+
+    def start(self):
+        self.p1.fight(self.p2)
+
 
 # a class can inherit from another class:
 # in this case, the DCSuperHero class inherits from the SuperHero class
@@ -95,9 +153,10 @@ class DCSuperHero(SuperHero):
     def get_description(self):
         return self.description + ' and they are part of the DC universe'
 
-
-superman = SuperHero('Superman', ['x-ray vision', 'flying', 'super strength'])
-
+superman = DCSuperHero('Superman', strength=93, powers=['x-ray vision', 'flying', 'super strength'])
+batman = DCSuperHero('Batman', strength=85, powers=['lots of money'])
+catwoman = DCSuperHero('Catwoman', strength=88, powers=['speed', 'agility'])
+robin = DCSuperHero('Robin', strength=75, powers=['agility'])
 
 
 # https://realpython.com/python3-object-oriented-programming/
